5ebc814046e9de306f5405c3b8f4e2a008aa7f28
Karelgit <huanghai@simpletech.com.cn>
2016-06-23 10:49:50 +0800

    convert fix_2016-6-23 10:49:40

---
+++ src/main/java/com/gy/wm/downloader/HtmlunitDownloader.java
@@ -0,0 +1,27 @@
+/*
+package com.gy.wm.downloader;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import us.codecraft.webmagic.Page;
+import us.codecraft.webmagic.Request;
+import us.codecraft.webmagic.Task;
+import us.codecraft.webmagic.downloader.Downloader;
+
+*/
+/**
+ * Created by Administrator on 2016/6/22.
+ *//*
+
+public class HtmlunitDownloader implements Downloader {
+    @Override
+    public Page download(Request request, Task task) {
+
+    }
+
+    @Override
+    public void setThread(int threadNum) {
+
+    }
+}
+*/
